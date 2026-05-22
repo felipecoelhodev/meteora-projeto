d299e260daa9c151dba929b9005a5a8b09adbc2f
felipecoelhodev <felipecoelhoflorenciotrab@gmail.com>
2026-05-22 10:05:29 -0300

    implementação da página home com SSR

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,42 @@
 import styles from "./page.module.css";
 import { Categorias } from "./components/Categorias";
 import { Produtos } from "./components/Produtos";
+import { API_BASE_URL, API_ENDPOINTS } from "../../lib/config";
+
+//Função para buscar categorias da API interna(BFF) - SSR PURO
+const fetchCategories = async () => {
+  const response = await fetch(`${API_BASE_URL}/${API_ENDPOINTS.CATEGORIES}`, {
+    cache: "force-cache",
+  });
+
+  if (!response.ok) {
+    throw new Error(
+      `Erro ao buscar categorias: ${response.status} ${response.statusText}`,
+    );
+  }
+
+  return await response.json();
+};
+
+const fetchProducts = async () => {
+  const response = await fetch(`${API_BASE_URL}/${API_ENDPOINTS.PRODUCTS}`, {
+    cache: "no-store",
+  });
+
+  if (!response.ok) {
+    throw new Error(
+      `Erro ao buscar produtos: ${response.status} ${response.statusText}`,
+    );
+  }
+
+  return await response.json();
+};
 
 export default async function Home() {
-  const categorias = [];
-  const produtos = [];
+  const [categorias, produtos] = await Promise.all([
+    fetchCategories(),
+    fetchProducts(),
+  ]);
 
   return (
     <div className={styles.page}>
@@ -15,3 +47,16 @@ export default async function Home() {
     </div>
   );
 }
+
+//Metadata para SEO
+export const metadata = {
+  title: "Meteora | Loja de Roupas",
+  description:
+    "Descubra as últimas tendencias em moda na Meteora. Camisetas, blusas, calçados em muito mais com qualidade e estilo.",
+  keywords: "moda, roupas, camisetas, bolsas, calçados, meteora",
+  openGraph: {
+    title: "Meteora - Loja de Roupas",
+    description: "As últimas tendências em moda você encontra aqui!",
+    type: "website",
+  },
+};
