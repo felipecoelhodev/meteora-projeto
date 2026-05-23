94e148b242098d142b8396b57ec48edcb3600bc8
felipecoelhodev <felipecoelhoflorenciotrab@gmail.com>
2026-05-23 10:01:51 -0300

    utilizando ISR

--- src/app/page.js
+++ src/app/page.js
@@ -1,41 +1,24 @@
 import styles from "./page.module.css";
 import { Categorias } from "./components/Categorias";
 import { Produtos } from "./components/Produtos";
-import { API_BASE_URL, API_ENDPOINTS } from "../../lib/config";
+import { fetchCategories, fetchProducts } from "../../lib/data-layer";
+import { unstable_cache } from "next/cache";
 
-//Função para buscar categorias da API interna(BFF) - SSR PURO
-const fetchCategories = async () => {
-  const response = await fetch(`${API_BASE_URL}/${API_ENDPOINTS.CATEGORIES}`, {
-    cache: "force-cache",
-  });
+// CATEGORIAS = SSG
+// PRODUTOS = ISR
 
-  if (!response.ok) {
-    throw new Error(
-      `Erro ao buscar categorias: ${response.status} ${response.statusText}`,
-    );
-  }
-
-  return await response.json();
-};
-
-const fetchProducts = async () => {
-  const response = await fetch(`${API_BASE_URL}/${API_ENDPOINTS.PRODUCTS}`, {
-    cache: "no-store",
-  });
-
-  if (!response.ok) {
-    throw new Error(
-      `Erro ao buscar produtos: ${response.status} ${response.statusText}`,
-    );
-  }
-
-  return await response.json();
-};
+const getCachedProducts = unstable_cache(
+  () => fetchProducts({ limit: 6 }),
+  ["products-home"],
+  {
+    revalidate: 10,
+  },
+);
 
 export default async function Home() {
   const [categorias, produtos] = await Promise.all([
     fetchCategories(),
-    fetchProducts(),
+    getCachedProducts(),
   ]);
 
   return (
